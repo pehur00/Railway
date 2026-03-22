6979063e2785aded3174e00ea262499bc7522dc9
pehur00 <95652813+pehur00@users.noreply.github.com>
2026-03-22 21:34:44 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const { PeerServer } = require('peer');
 const server = PeerServer({
   port: process.env.PORT || 9000,
   path: '/',
-  allow_discovery: true,
   corsOptions: { origin: '*' }
 });
-server.on('connection', (client) => console.log('connected:', client.getId()));
-server.on('disconnect', (client) => console.log('disconnected:', client.getId()));
-console.log('PeerJS server running');
+console.log('PeerJS server running on port', process.env.PORT || 9000);
